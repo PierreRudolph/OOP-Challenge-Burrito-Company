210e7499aa0637fcbdc8eedf67897cca57090948
Pierre Lettner <PLettner@hotmail.de>
2024-03-06 10:26:52 +0100

    add product list,abillity to add products

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ class Product {
     name = "";
     amount = 0;
     prize = 0;
-    constructor(name, amount, prize) {
+    id = 0;
+    constructor(name, amount, prize, id) {
         if (name) {
             this.name = name;
         }
@@ -22,6 +23,7 @@ class Product {
         if (prize) {
             this.prize = prize;
         }
+        this.id = id ? id : '';
     }
 }
 
@@ -32,20 +34,49 @@ let products = [
     {
         name: 'Cheese Burrito',
         amount: 3,
-        prize: 8.50
+        prize: 8.50,
+        id: 1234
     },
     {
         name: 'Salad Burrito',
         amount: 4,
-        prize: 3.50
+        prize: 3.50,
+        id: 1345
     },
     {
         name: 'Pork Burrito',
         amount: 3,
-        prize: 6.50
+        prize: 6.50,
+        id: 1456
     },
 ]
 
+function addProduct(id) {
+    let newProduct = '';
+    products.forEach((product) => {
+        newProduct = (product.id == id) ? newProduct = product : newProduct;
+    })
+    console.log(newProduct)
+    if (basket.products.length == 0) {
+        basket.products.push(new Product(newProduct.name, 1, newProduct.prize, newProduct.id))
+        console.log(basket.products)
+        loadBasket();
+        return
+    }
+
+
+    basket.products.forEach((product) => {
+        if (newProduct.id == product.id) {
+            console.log('amount called')
+            product.amount = product.amount + 1;
+        } else {
+            console.log('push called')
+            basket.products.push(new Product(newProduct.name, 1, newProduct.prize, newProduct.id))
+        }
+    })
+    console.log(basket.products)
+    loadBasket();
+}
 
 function createBasket() {
     clearTableBody();
@@ -82,10 +113,10 @@ function loadBasket() {
         totalAmount = totalAmount + prizeSum;
     });
 
-    if (!percent) {
+    if (!percent && rabatt > 0) {
         rabattAmount = totalAmount - rabatt
     }
-    if (percent) {
+    if (percent && rabatt > 0) {
         rabattPecent = totalAmount * rabatt / 100;
         rabattAmount = totalAmount - rabattPecent;
     }
@@ -96,15 +127,15 @@ function loadBasket() {
 function innerHTMLTotalPrize(totalAmount, rabattAmount) {
     let tableFooter = getHTMLElem('table-footer');
     if (!percent) {
-        rabatt = formatNumber(rabatt);
+        rabattToString = formatNumber(rabatt);
     } else {
-        rabatt = rabatt + '%';
+        rabattToString = rabatt + '%';
     }
     tableFooter.innerHTML +=/*html*/`
     <tr>
         <th>Rabatt:</th>
         <td></td>
-        <td>${rabatt}</td>
+        <td>${rabattToString}</td>
         <td></td>
     </tr>
     <tr>
@@ -146,12 +177,11 @@ function makeRabatt() {
 
 
 function loadPreBuildBasket() {
-    //clearBasket();
-    products.forEach((product) => {
-        let newProduct = new Product(product.name, product.amount, product.prize);
-        basket.products.push(newProduct)
-    })
-    loadBasket();
+    // products.forEach((product) => {
+    //     let newProduct = new Product(product.name, product.amount, product.prize);
+    //     basket.products.push(newProduct);
+    // })
+    // loadBasket();
 }
 
 function fillRabattInput(rabattCode) {
